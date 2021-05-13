d59535927edeeec7a585924def050da6c1713ef7
dextr0stat <77321060+dextr0stat@users.noreply.github.com>
2021-05-13 07:35:42 +0530

    Update test3.py

--- A00/test3.py
+++ A00/test3.py
@@ -7,11 +7,16 @@ import ghpythonlib.components as ghc
 mes= rg.Mesh()
 #print (mes.Vertices)
 
+#A mesh has vertices and faces
+# You add the points as vertices through a for loop
 for i in x:
     mes.Vertices.Add(i)
 
+b= mes.Vertices
 print len(mes.Vertices)
 
+#then you add the faces through AddFace
+#https://developer.rhino3d.com/api/RhinoCommon/html/T_Rhino_Geometry_Collections_MeshFaceList.htm
 mes.Faces.AddFace(0,1,2,3)
 
 a= mes
